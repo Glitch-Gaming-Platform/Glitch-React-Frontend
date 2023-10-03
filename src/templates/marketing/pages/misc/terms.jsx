1be84f478eed92320de9670cf0afed5a462eb8a6
Devin Dixon <devindixon@Devins-MacBook-Pro.local>
2023-10-03 14:58:14 -0400

    Updating terms of service

--- src/templates/marketing/pages/misc/terms.jsx
+++ src/templates/marketing/pages/misc/terms.jsx
@@ -18,34 +18,53 @@ class TermsPage extends Component {
                 <Header />
                 <PageHeader title={'Terms Of Service'} curPage={'About Us'} />
                 <div className="container mt-5">
-                    <h2 className="text-center mb-4">TERMS OF SERVICE</h2>
+                    <h1 className="text-center mb-4">TERMS OF SERVICE</h1>
+                    <p>This Agreement explains your rights and obligations in using the Glitch platform (hereafter referred to as "the Services"). By clicking “Accept”, submitting information, or continuing to use the Services, you confirm that you have read and understand this Agreement.</p>
 
-                    <p>This Agreement explains your rights and obligations in using the Glitck platform (hereafter referred to as "the Services"). By clicking “Accept”, submitting information, or continuing to use the Services, you confirm that you have read and understand this Agreement.</p>
+                    <h5>1. AS-IS</h5>
+                    <p>The Services are provided "as-is". Unless prohibited by law, you agree that accessing the Service is at your sole risk. We disclaim all warranties, express or implied, regarding the Services.</p>
 
-                    <p><strong>AS-IS.</strong> The Services are provided "as-is". Unless prohibited by law, you agree that accessing the Service is at your sole risk. We disclaim all warranties, express or implied, regarding the Services.</p>
+                    <h5>2. SUBJECT TO CHANGE</h5>
+                    <p>The Services can be changed or discontinued at our discretion without prior notice.</p>
 
-                    <p><strong>SUBJECT TO CHANGE.</strong> The Services can be changed or discontinued at our discretion without prior notice.</p>
+                    <h5>3. USER CONDUCT</h5>
+                    <p>You are responsible for all the content you post, stream, or otherwise make available through the Services. You agree not to use the Services for any unlawful purposes or to conduct any unlawful activity, including, but not limited to fraud, embezzlement, money laundering or insider trading.</p>
 
-                    <p><strong>DISPUTE RESOLUTION – VENUE.</strong> Any disputes will be resolved in Connecticut unless otherwise agreed upon by Invirtu Labs, Inc.</p>
+                    <h5>4. INTELLECTUAL PROPERTY</h5>
+                    <p>Unless otherwise specified, all materials appearing on the Services, including the text, design, logos, graphics, icons and images, as well as the selection, assembly and arrangement thereof, are the sole property of Invirtu Labs, Inc., or its licensors. You may not use, reproduce, modify or distribute such content without our express written permission.</p>
 
-                    <p><strong>DISPUTE RESOLUTION – ARBITRATION, NO CLASS ACTIONS.</strong> You agree to individual arbitration through the American Arbitration Association (“AAA”). Class actions are not permitted.</p>
+                    <h5>5. DISPUTE RESOLUTION – VENUE</h5>
+                    <p>Any disputes will be resolved in Connecticut unless otherwise agreed upon by Invirtu Labs, Inc.</p>
 
-                    <p><strong>YOUR LIABILITY.</strong> You are responsible for your use of the Services. You agree to indemnify us against any damages arising from your use.</p>
+                    <h5>6. DISPUTE RESOLUTION – ARBITRATION, NO CLASS ACTIONS</h5>
+                    <p>You agree to individual arbitration through the American Arbitration Association (“AAA”). Class actions are not permitted.</p>
 
-                    <p><strong>STANDARD OF CARE.</strong> Our sole obligation is to provide the Services "as-is". Unless there's gross negligence or willful misconduct, we're not liable to you or any third party.</p>
+                    <h5>7. YOUR LIABILITY</h5>
+                    <p>You are responsible for your use of the Services. You agree to indemnify us against any damages arising from your use.</p>
 
-                    <p><strong>LIMITATIONS ON LIABILITY.</strong> Any liability on our part will be limited to actual damages. No consequential, special, punitive, or indirect damages can be awarded, even if requested.</p>
+                    <h5>8. STANDARD OF CARE</h5>
+                    <p>Our sole obligation is to provide the Services "as-is". Unless there's gross negligence or willful misconduct, we're not liable to you or any third party.</p>
 
-                    <p><strong>WE’RE NOT RESPONSIBLE FOR ALL CIRCUMSTANCES.</strong> We're not liable for issues beyond our control, like network interruptions, ISP problems, or third-party attacks.</p>
+                    <h5>9. LIMITATIONS ON LIABILITY</h5>
+                    <p>Any liability on our part will be limited to actual damages. No consequential, special, punitive, or indirect damages can be awarded, even if requested.</p>
 
-                    <h3>EXTERNAL FACTORS</h3>
+                    <h5>10. EXTERNAL FACTORS</h5>
                     <p>Issues beyond our control, including network interruptions, are not our responsibility.</p>
 
-                    <p><strong>TERMINATION BY US.</strong> We can terminate this Agreement at our discretion and may do so without notifying you in advance.</p>
+                    <h5>11. TERMINATION BY US</h5>
+                    <p>We can terminate this Agreement at our discretion and may do so without notifying you in advance.</p>
 
-                    <p><strong>AMENDMENTS.</strong> We can change the Services or this Agreement. We'll notify you of significant changes. Continued use of the Services means you accept those changes.</p>
+                    <h5>12. AMENDMENTS</h5>
+                    <p>We can change the Services or this Agreement. We'll notify you of significant changes. Continued use of the Services means you accept those changes.</p>
 
+                    <h5>13. ENTIRE AGREEMENT</h5>
+                    <p>This Agreement constitutes the entire agreement between you and Invirtu Labs, Inc. regarding the use of the Services.</p>
 
+                    <h5>14. SEVERABILITY</h5>
+                    <p>If any provision in this Agreement is held invalid or unenforceable, that provision shall be construed in a manner consistent with applicable law to reflect the original intentions of the parties, and the remaining provisions shall remain in full force and effect.</p>
+
+                    <h5>15. GOVERNING LAW</h5>
+                    <p>This Agreement shall be governed by and construed under the laws of the state of Connecticut without regard to its conflict of law principles.</p>
                 </div>
             </Fragment>
         );
