c4fdc58c4a02adf6db06c0675120fb449e23463b
Devin Dixon <devindixon@Devins-MacBook-Pro.local>
2023-10-02 20:29:00 -0400

    Adding clause to terms

--- src/templates/marketing/pages/misc/terms.jsx
+++ src/templates/marketing/pages/misc/terms.jsx
@@ -20,13 +20,13 @@ class TermsPage extends Component {
                 <div className="container mt-5">
                     <h2 className="text-center mb-4">TERMS OF SERVICE</h2>
 
-                    <p>This Agreement explains your rights and obligations in using the TikTok platform (hereafter referred to as "the Services"). By clicking “Accept”, submitting information, or continuing to use the Services, you confirm that you have read and understand this Agreement.</p>
+                    <p>This Agreement explains your rights and obligations in using the Glitck platform (hereafter referred to as "the Services"). By clicking “Accept”, submitting information, or continuing to use the Services, you confirm that you have read and understand this Agreement.</p>
 
                     <p><strong>AS-IS.</strong> The Services are provided "as-is". Unless prohibited by law, you agree that accessing the Service is at your sole risk. We disclaim all warranties, express or implied, regarding the Services.</p>
 
                     <p><strong>SUBJECT TO CHANGE.</strong> The Services can be changed or discontinued at our discretion without prior notice.</p>
 
-                    <p><strong>DISPUTE RESOLUTION – VENUE.</strong> Any disputes will be resolved in Connecticut unless otherwise agreed upon by TikTok.</p>
+                    <p><strong>DISPUTE RESOLUTION – VENUE.</strong> Any disputes will be resolved in Connecticut unless otherwise agreed upon by Invirtu Labs, Inc.</p>
 
                     <p><strong>DISPUTE RESOLUTION – ARBITRATION, NO CLASS ACTIONS.</strong> You agree to individual arbitration through the American Arbitration Association (“AAA”). Class actions are not permitted.</p>
 
@@ -45,15 +45,6 @@ class TermsPage extends Component {
 
                     <p><strong>AMENDMENTS.</strong> We can change the Services or this Agreement. We'll notify you of significant changes. Continued use of the Services means you accept those changes.</p>
 
-                    <h3 className="text-center mb-4 mt-5">PRIVACY STATEMENT</h3>
-
-                    <p>By accepting these terms, you also accept our Privacy Statement. We respect and aim to protect your privacy.</p>
-
-                    <p><strong>INFORMATION COLLECTED.</strong> Your information stays confidential with TikTok. We have measures to protect your data. We don't share personal identifiable info but may share user activity data with third parties.</p>
-
-                    <p><strong>HOW THE INFORMATION IS USED.</strong> We analyze user data to improve our Services. We might contact you with updates or offerings related to TikTok.</p>
-
-                    <p><strong>UPDATING OR DELETING INFORMATION.</strong> You can modify or delete your data by contacting us.</p>
 
                 </div>
             </Fragment>
