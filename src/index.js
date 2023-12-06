474c7b3324a93aa13bb90d1218a1d50884ada718
Devin Dixon <devindixon@Devins-MacBook-Pro.local>
2023-12-06 14:42:01 -0500

    Adding snap

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { BrowserRouter } from "react-router-dom";
-
 import reportWebVitals from './reportWebVitals';
-
 import Glitch from 'glitch-javascript-sdk';
 
-
 Glitch.config.Config.setBaseUrl(process.env.REACT_APP_API_URL, true);
-
 Glitch.config.Config.setRootDomain(process.env.REACT_APP_SITE_DOMAIN);
-
-//document.domain = Glitch.config.Config.getRootDomain();
-
 Glitch.config.Config.setAuthToken(Glitch.util.Storage.getAuthToken());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-root.render(
+// Check if the app is running in a browser
+if (rootElement.hasChildNodes()) {
+  // Hydrate the app if it's already rendered to static HTML by react-snap
+  ReactDOM.hydrateRoot(
+    rootElement,
     <BrowserRouter>
       <App />
     </BrowserRouter>
-);
+  );
+} else {
+  // Render the app normally
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
